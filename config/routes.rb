e7cc7fa6782beff50b835371ec23c6c9a1e1afa3
Jonathan Viney <jonathan.viney@gmail.com>
2013-02-03 13:55:25 +1300

    CSP endpoint only accepts POST requests. Fixes Rails 4 compatibility.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  match SecureHeaders::ContentSecurityPolicy::FF_CSP_ENDPOINT => "content_security_policy#scribe"
+  post SecureHeaders::ContentSecurityPolicy::FF_CSP_ENDPOINT => "content_security_policy#scribe"
 end
