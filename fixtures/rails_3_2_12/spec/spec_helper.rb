a8c789f1ef856fd3bcb5195fd2bb45d37ab1627b
Neil Matatall <neilm@twitter.com>
2013-02-18 09:40:09 -0800

    spork issues?

--- fixtures/rails_3_2_12/spec/spec_helper.rb
+++ fixtures/rails_3_2_12/spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
-Spork.prefork do
+# Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
@@ -12,7 +12,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
-end
+# end
 
 Spork.each_run do
 
