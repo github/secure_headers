deab292e4db91f4ef006c0763b4b39027729fc73
Neil Matatall <neilm@twitter.com>
2013-02-18 09:00:46 -0800

    Add missing fixture files

--- fixtures/rails_3_2_12/config/application.rb
+++ fixtures/rails_3_2_12/config/application.rb
@@ -1,11 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-# require "active_record/railtie"
+require "active_record/railtie"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "active_resource/railtie"
-# require "sprockets/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
