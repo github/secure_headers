9908ae6fa15f32f1407268e6c5da287f5890108d
Neil Matatall <neilm@twitter.com>
2014-08-07 14:43:45 -0700

    goodbye spork and simplecov, tired of you

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,10 @@
 require 'rubygems'
-require 'spork'
+require 'rspec'
 
-unless Spork.using_spork?
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "spec"
-  end
-end
-
-Spork.prefork do
-  require 'rspec'
-end
-
-Spork.each_run do
-  require File.join(File.dirname(__FILE__), '..', 'lib', 'secure_headers')
-  require File.join(File.dirname(__FILE__), '..', 'app', 'controllers', 'content_security_policy_controller')
-  include ::SecureHeaders::StrictTransportSecurity::Constants
-  include ::SecureHeaders::ContentSecurityPolicy::Constants
-  include ::SecureHeaders::XFrameOptions::Constants
-  include ::SecureHeaders::XXssProtection::Constants
-  include ::SecureHeaders::XContentTypeOptions::Constants
-end
-
+require File.join(File.dirname(__FILE__), '..', 'lib', 'secure_headers')
+require File.join(File.dirname(__FILE__), '..', 'app', 'controllers', 'content_security_policy_controller')
+include ::SecureHeaders::StrictTransportSecurity::Constants
+include ::SecureHeaders::ContentSecurityPolicy::Constants
+include ::SecureHeaders::XFrameOptions::Constants
+include ::SecureHeaders::XXssProtection::Constants
+include ::SecureHeaders::XContentTypeOptions::Constants
