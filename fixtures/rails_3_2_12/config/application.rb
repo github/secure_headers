76eb76fca3088ec6b936628beabfe2c8ac4f3f08
Steve Agalloco <steve.agalloco@gmail.com>
2014-08-28 10:31:12 -0400

    remove ActiveRecord dependency from integration tests

--- fixtures/rails_3_2_12/config/application.rb
+++ fixtures/rails_3_2_12/config/application.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
+# require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
+# require "action_mailer/railtie"
+# require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -57,7 +57,7 @@ module Rails3212
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
