829e41abfb0684ded1f8f1bd9cdd14a1fb22d223
Neil Matatall <neilm@twitter.com>
2014-10-13 17:40:47 -0700

    fix tests on 1.8.7

--- fixtures/rails_3_2_12_no_init/spec/controllers/other_things_controller_spec.rb
+++ fixtures/rails_3_2_12_no_init/spec/controllers/other_things_controller_spec.rb
@@ -22,40 +22,41 @@ describe OtherThingsController, :type => :controller do
       _, @env = app.call(request)
     end
     it "sets the X-XSS-Protection header" do
-      get :index
+      get '/'
       expect(@env['X-XSS-Protection']).to eq('1; mode=block')
     end
 
     it "sets the X-Frame-Options header" do
-      get :index
+      get '/'
       expect(@env['X-Frame-Options']).to eq('SAMEORIGIN')
     end
 
     it "sets the CSP header with a local reference to a nonce" do
-      get :index
+      get '/'
       middleware = ::SecureHeaders::ContentSecurityPolicy::Middleware.new(app)
       _, env = middleware.call(request(@env))
       expect(env['Content-Security-Policy-Report-Only']).to match(/default-src 'self'; img-src 'self' data:; script-src 'self' 'nonce-[a-zA-Z0-9\+\/=]{44}' 'unsafe-inline'; report-uri somewhere;/)
     end
 
     it "sets the Strict-Transport-Security header" do
-      get :index
+      get '/'
       expect(@env['Strict-Transport-Security']).to eq("max-age=315576000")
     end
 
     it "sets the X-Download-Options header" do
-      get :index
+      get '/'
       expect(@env['X-Download-Options']).to eq('noopen')
     end
 
     it "sets the X-Content-Type-Options header" do
-      get :index
+      get '/'
       expect(@env['X-Content-Type-Options']).to eq("nosniff")
     end
 
     context "using IE" do
       it "sets the X-Content-Type-Options header" do
-        get :index
+        debugger
+        get '/'
         expect(@env['X-Content-Type-Options']).to eq("nosniff")
       end
     end
