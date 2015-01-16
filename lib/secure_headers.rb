da0f02751cbe1e85c6ac3bbc5ce1a1fa51d3a93a
Nate Collings <nate@natecollings.com>
2015-01-16 12:51:11 -0700

    set the header in default_headers if defined there

--- lib/secure_headers.rb
+++ lib/secure_headers.rb
@@ -157,10 +157,18 @@ module SecureHeaders
     def set_header(name_or_header, value=nil)
       if name_or_header.is_a?(Header)
         header = name_or_header
-        response.headers[header.name] = header.value
+        if default_header?(header.name)
+          Rails.application.config.action_dispatch.default_headers[header.name] = header.value
+        else
+          response.headers[header.name] = header.value
+        end
       else
         response.headers[name_or_header] = value
       end
+    end
+
+    def default_header?(name)
+      Rails.application.config.action_dispatch.default_headers.has_key?(name)
     end
   end
 end
