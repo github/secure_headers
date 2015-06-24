9b77a7cbb9756cf3a3a9e38f298a0529580406df
Neil Matatall <oreoshake@users.noreply.github.com>
2015-06-24 11:50:25 -0700

    only use nonces for browsers that support it or fall back gracefully

--- fixtures/rails_4_1_8/spec/controllers/other_things_controller_spec.rb
+++ fixtures/rails_4_1_8/spec/controllers/other_things_controller_spec.rb
@@ -13,7 +13,7 @@ describe OtherThingsController, :type => :controller do
     options = opts.merge(
       {
         'HTTPS' => 'on',
-        'HTTP_USER_AGENT' => "Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0"
+        'HTTP_USER_AGENT' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 1084) AppleWebKit/537.22 (KHTML like Gecko) Chrome/25.0.1364.99 Safari/537.22"
       }
     )
 
