6cef8c7e0ab34d43cd7291c7c3383c8cd3728b8d
Neil Matatall <oreoshake@users.noreply.github.com>
2016-01-06 12:02:45 -1000

    oops, move deprecation warning out of test and into code

--- fixtures/rails_3_2_22_no_init/app/controllers/other_things_controller.rb
+++ fixtures/rails_3_2_22_no_init/app/controllers/other_things_controller.rb
@@ -9,7 +9,6 @@ class OtherThingsController < ApplicationController
   end
 
   def secure_header_options_for(header, options)
-    warn "[DEPRECATION] secure_header_options_for will not be supported in secure_headers 3.x."
     if params[:action] == "other_action"
       if header == :csp
         options.merge(:style_src => "'self'")
