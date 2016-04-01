8e18257bffbb47d76ae6902c85e15d3d2198d454
Neil Matatall <oreoshake@github.com>
2016-04-01 13:08:16 -1000

    oops, hash_source method regressed

--- lib/secure_headers/hash_helper.rb
+++ lib/secure_headers/hash_helper.rb
@@ -1,7 +1,9 @@
+require 'base64'
+
 module SecureHeaders
   module HashHelper
     def hash_source(inline_script, digest = :SHA256)
-      base64_hashed_content = Digest.const_get(digest).hexdigest(inline_script)].pack("H*")].pack("m").chomp
+      base64_hashed_content = Base64.encode64(Digest.const_get(digest).digest(inline_script)).chomp
       "'#{digest.to_s.downcase}-#{base64_hashed_content}'"
     end
   end
