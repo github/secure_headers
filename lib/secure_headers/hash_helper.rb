6b5324ff393d6d89bc24a9f5c6cfe2da4c5860a8
Neil Matatall <oreoshake@github.com>
2016-04-01 11:06:01 -1000

    make hash_source method less confusing

--- lib/secure_headers/hash_helper.rb
+++ lib/secure_headers/hash_helper.rb
@@ -1,7 +1,8 @@
 module SecureHeaders
   module HashHelper
     def hash_source(inline_script, digest = :SHA256)
-      ["'", [digest.to_s.downcase, "-", [[Digest.const_get(digest).hexdigest(inline_script)].pack("H*")].pack("m").chomp].join, "'"].join
+      base64_hashed_content = Digest.const_get(digest).hexdigest(inline_script)].pack("H*")].pack("m").chomp
+      "'#{digest.to_s.downcase}-#{base64_hashed_content}'"
     end
   end
 end
