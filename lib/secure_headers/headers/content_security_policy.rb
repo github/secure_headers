ddec69584c16bbf377878de5a31145b5e6ef7f31
Neil Matatall <oreoshake@users.noreply.github.com>
2020-01-21 08:44:58 -1000

    escape semicolons by replacing them with spaces
    See https://github.com/twitter/secure_headers/issues/418

--- lib/secure_headers/headers/content_security_policy.rb
+++ lib/secure_headers/headers/content_security_policy.rb
@@ -138,8 +138,14 @@ module SecureHeaders
       end
 
       if source_list != OPT_OUT && source_list && source_list.any?
-        normalized_source_list = minify_source_list(directive, source_list)
-        [symbol_to_hyphen_case(directive), normalized_source_list].join(" ")
+        minified_source_list = minify_source_list(directive, source_list).join(" ")
+
+        if minified_source_list.include?(";")
+          Kernel.warn("#{directive} contains a ; in '#{minified_source_list}' which will raise an error in future versions. It has been replaced with a blank space.")
+        end
+
+        escaped_source_list = minified_source_list.gsub(";", " ")
+        [symbol_to_hyphen_case(directive), escaped_source_list].join(" ").strip
       end
     end
 
